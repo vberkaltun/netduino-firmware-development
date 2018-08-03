0152e71ad6513b8149c7497fb59c91dd1ccf11f9
Berk Altun <vberkaltun@msn.com>
2018-08-03 16:15:05 +0300

    Master structure was deleted, it is better looking now

--- Libraries/netduinoMaster/netduinoMaster/Interface/IMaster.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace netduinoMaster
-{
-    public interface IMaster
-    {
-        SFunctionArray Function { get; set; }
-        string Receive { get; set; }
-        string Transmit { get; set; }
-    }
-}
