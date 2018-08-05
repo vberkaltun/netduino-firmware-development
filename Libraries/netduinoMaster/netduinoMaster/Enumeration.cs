157ce1d706321fbd6d98c03007ab4fbc2c1f5452
Berk Altun <vberkaltun@msn.com>
2018-08-05 11:12:49 +0300

    ETimer variable was deleted, unnecessary

--- Libraries/netduinoMaster/netduinoMaster/Enumeration.cs
+++ Libraries/netduinoMaster/netduinoMaster/Enumeration.cs
@@ -3,5 +3,4 @@ namespace netduinoMaster
     public enum EHandshake { Unknown, Ready };
     public enum ECommunication { Idle, Continue, End };
     public enum ENotify { Offline, Online, Unconfirmed, Confirmed };
-    public enum ETimer { I2C, Function, WiFi };
 }
