182e66c9d72a2c88e0aad66ff7334c7bf88a7c90
Berk Altun <vberkaltun@msn.com>
2018-08-05 10:30:19 +0300

    Now, ScanSlaves function has I2CDevice parameter

--- Libraries/intelliPWR/MasterScanner/Interface/IMasterScanner.cs
+++ Libraries/intelliPWR/MasterScanner/Interface/IMasterScanner.cs
@@ -2,7 +2,7 @@
 {
     public interface IMasterScanner
     {
-        void ScanSlaves();
+        void ScanSlaves(Microsoft.SPOT.Hardware.I2CDevice Device);
         bool SetRange(byte startAddress, byte stopAddress);
         void ResetRange();
         bool IsConnected(byte address);
