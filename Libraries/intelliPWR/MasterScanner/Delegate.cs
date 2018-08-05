31cb3104d06ced93ebcd07b74951e961ffc7cd2d
Berk Altun <vberkaltun@msn.com>
2018-08-05 10:31:38 +0300

    Return type of delegate was changed as byte[], string is not useful

--- Libraries/intelliPWR/MasterScanner/Delegate.cs
+++ Libraries/intelliPWR/MasterScanner/Delegate.cs
@@ -5,12 +5,12 @@ namespace intelliPWR.MasterScanner
     /// </summary>
     /// <param name="array">An array of connected device list.</param>
     /// <param name="count">Size of connected device.</param>
-    public delegate void ConnectedEventHandler(string[] array, byte count);
+    public delegate void ConnectedEventHandler(byte[] array, byte count);
 
     /// <summary>
     /// Delegate that defines event handler for disconnected.
     /// </summary>
     /// <param name="array">An array of disconnected device list.</param>
     /// <param name="count">Size of disconnected device.</param>
-    public delegate void DisconnectedEventHandler(string[] array, byte count);
+    public delegate void DisconnectedEventHandler(byte[] array, byte count);
 }
